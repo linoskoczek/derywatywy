92aa4031d041d586c9b96fa28d8325656c4eb37a
linoskoczek <linoskoczek@protonmail.com>
2020-05-22 23:07:53 +0200

    added requirements, code cleanup

--- projekt.py
+++ projekt.py
@@ -1,6 +1,5 @@
 import sklearn as sk
-import pandas as pd 
-import morfeusz2
+import pandas as pd
 from hyphen import Hyphenator
 from sklearn import preprocessing
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
@@ -14,10 +13,13 @@ data = pd.read_csv('dane.csv', sep=';')
 data = data[['child_lemma', 'parent_lemma']]
 data = data.drop_duplicates()
 h_pl = Hyphenator('pl_PL')
-pd_list = [[h_pl.syllables(c[0]), h_pl.syllables(c[1]), c[0], c[1]] for c in data.values]
-pd_list = [[p[0][-1], p[1][-1], p[2], p[3]] for p in pd_list if p[0] != '' and p[0] != []]
+pd_list = [[h_pl.syllables(c[0]), h_pl.syllables(
+    c[1]), c[0], c[1]] for c in data.values]
+pd_list = [[p[0][-1], p[1][-1], p[2], p[3]]
+           for p in pd_list if p[0] != '' and p[0] != []]
 df = pd.DataFrame().from_records(pd_list)
 df.columns = ['ms', 'fs', 'm', 'f']
+
 
 def encode_df(dataframe, le):
     dataframe = dataframe.copy()
@@ -25,11 +27,13 @@ def encode_df(dataframe, le):
         dataframe[column] = le[column].fit_transform(dataframe[column])
     return dataframe
 
+
 def decode_df(dataframe, le):
     dataframe = dataframe.copy()
     for column in dataframe.columns:
         dataframe[column] = le[column].inverse_transform(dataframe[column])
     return dataframe
+
 
 fs_le = preprocessing.LabelEncoder()
 f_le = preprocessing.LabelEncoder()
@@ -60,10 +64,10 @@ for neigh in algs:
     neigh.fit(X, y)
 
     predicted = neigh.predict(test[['m', 'ms']])
-    pred=pd.DataFrame(predicted)
+    pred = pd.DataFrame(predicted)
     pred.columns = ['fs']
     pred['fs'] = pred['fs'].apply(np.int64)
-    pred = pred.rename(index=dict(zip(pred.index,test.index)))
+    pred = pred.rename(index=dict(zip(pred.index, test.index)))
     pred['m'] = test['m']
     pred['ms'] = test['ms']
     pred['c_f'] = test['f']
@@ -71,7 +75,8 @@ for neigh in algs:
 
     de = decode_df(pred, dict_le)
 
-    print("Score for", type(neigh).__name__, accuracy_score(pred['fs'], test['fs']))
+    print("Score for", type(neigh).__name__,
+          accuracy_score(pred['fs'], test['fs']))
 
 # for i in range(1, 500, 20):
 #     print(de[i:i+20])
