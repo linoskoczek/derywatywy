053ca409b87fbb7a77e9432220b5eb73cb8a230a
linoskoczek <linoskoczek@protonmail.com>
2020-06-13 23:04:35 +0200

    added checking for best num of letters

--- projekt.py
+++ projekt.py
@@ -12,14 +12,6 @@ import operator
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('dane.csv', sep=';')
-data = data[['child_lemma', 'parent_lemma']]
-data = data.drop_duplicates()
-h_pl = Hyphenator('pl_PL')
-pd_list = [[c[0][-5:], c[1][-5:], c[0], c[1]] for c in data.values]
-df = pd.DataFrame().from_records(pd_list)
-df.columns = ['ms', 'fs', 'm', 'f']
-
 
 def encode_df(dataframe, le):
     dataframe = dataframe.copy()
@@ -34,23 +26,26 @@ def decode_df(dataframe, le):
         dataframe[column] = le[column].inverse_transform(dataframe[column])
     return dataframe
 
+def learn_model_and_test(alg, dfn, train, test, dict_le):
+    X = dfn[['m', 'ms']]
+    y = dfn[['fs']]
+    alg.fit(X, np.ravel(y))
 
-fs_le = preprocessing.LabelEncoder()
-f_le = preprocessing.LabelEncoder()
+    predicted = alg.predict(test[['m', 'ms']])
+    pred = pd.DataFrame(predicted)
+    pred.columns = ['fs']
+    pred['fs'] = pred['fs'].apply(np.int64)
+    pred = pred.rename(index=dict(zip(pred.index, test.index)))
+    pred['m'] = test['m']
+    pred['ms'] = test['ms']
+    pred['c_f'] = test['f']
+    pred['c_fs'] = test['fs']
 
-dict_le = {
-    'm': preprocessing.LabelEncoder(),
-    'ms': preprocessing.LabelEncoder(),
-    'fs': fs_le,
-    'c_fs': fs_le,
-    'f': f_le,
-    'c_f': f_le
-}
-dfn = encode_df(df, dict_le)
+    decoded_results = decode_df(pred, dict_le)
+    score = accuracy_score(pred['fs'], test['fs'])
+    return score, decoded_results
 
-train, test = train_test_split(dfn, test_size=0.2)
-
-def find_best_algorithm():
+def find_best_algorithm_and_return_result(dfn, train, test, dict_le):
     best_results = None
     best_score = 0
 
@@ -63,24 +58,8 @@ def find_best_algorithm():
     ]
 
     scores = {}
-
     for alg in algs:
-        X = dfn[['m', 'ms']]
-        y = dfn[['fs']]
-        alg.fit(X, np.ravel(y))
-
-        predicted = alg.predict(test[['m', 'ms']])
-        pred = pd.DataFrame(predicted)
-        pred.columns = ['fs']
-        pred['fs'] = pred['fs'].apply(np.int64)
-        pred = pred.rename(index=dict(zip(pred.index, test.index)))
-        pred['m'] = test['m']
-        pred['ms'] = test['ms']
-        pred['c_f'] = test['f']
-        pred['c_fs'] = test['fs']
-
-        decoded_results = decode_df(pred, dict_le)
-        score = accuracy_score(pred['fs'], test['fs'])
+        score, decoded_results = learn_model_and_test(alg, dfn, train, test, dict_le)
         scores[score] = decoded_results
         print('{:>15} {}'.format(type(alg).__name__, score))
         if score > best_score:
@@ -89,6 +68,7 @@ def find_best_algorithm():
 
     return best_results
 
+
 def calc_str_offset(str1, str2):
     f = str1.find(str2[0])
     if f != -1:
@@ -96,19 +76,58 @@ def calc_str_offset(str1, str2):
     else:
         return len(str1)
 
+def main():
+    best_letter_number = 0
+    best_score = 0
+    best_results = None
+    
+    data = pd.read_csv('dane.csv', sep=';')
+    data = data[['child_lemma', 'parent_lemma']]
+    data = data.drop_duplicates()
 
-decoded_results = find_best_algorithm()
-mfs = list(decoded_results[['m', 'ms', 'fs']].values)
+    for letter_number in range(1, 20):
+        pd_list = [[c[0][-letter_number:], c[1][-letter_number:], c[0], c[1]] for c in data.values]
+        df = pd.DataFrame().from_records(pd_list)
+        df.columns = ['ms', 'fs', 'm', 'f']
 
-final_results = []
-for r in mfs:
-    m = r[0][:-calc_str_offset(r[1], r[2])]
-    final_results.append(m + r[2])
-    #print(r[0], '->', m + r[2])
+        fs_le = preprocessing.LabelEncoder()
+        f_le = preprocessing.LabelEncoder()
 
-decoded_results['final_results'] = final_results
-decoded_results['guess_ok'] = np.where(decoded_results['final_results'] == decoded_results['c_f'], True, False)
-count_true = decoded_results.guess_ok[decoded_results.guess_ok == True].count()
-count_all = len(decoded_results.index)
-print(decoded_results[['m', 'final_results', 'guess_ok']].head(-5))
-print("\nFinal accuracy:", count_true, "/", count_all, '->', count_true/count_all * 100, '%')
+        dict_le = {
+            'm': preprocessing.LabelEncoder(),
+            'ms': preprocessing.LabelEncoder(),
+            'fs': fs_le,
+            'c_fs': fs_le,
+            'f': f_le,
+            'c_f': f_le
+        }
+        dfn = encode_df(df, dict_le)
+        print('\n[TEST FOR LEARNING WITH LAST', letter_number, 'LETTERS]')
+
+        train, test = train_test_split(dfn, test_size=0.2)
+
+        decoded_results = find_best_algorithm_and_return_result(dfn, train, test, dict_le)
+        mfs = list(decoded_results[['m', 'ms', 'fs']].values)
+
+        final_results = []
+        for r in mfs:
+            m = r[0][:-calc_str_offset(r[1], r[2])]
+            final_results.append(m + r[2])
+
+        decoded_results['final_results'] = final_results
+        decoded_results['guess_ok'] = np.where(
+            decoded_results['final_results'] == decoded_results['c_f'], True, False)
+        count_true = decoded_results.guess_ok[decoded_results.guess_ok == True].count()
+        count_all = len(decoded_results.index)
+        score = count_true/count_all * 100
+        if score > best_score:
+            best_score = score
+            best_letter_number = letter_number
+            best_results = decoded_results
+        print("Result:", score, '%')
+        
+    return count_true, count_all, best_score, best_letter_number, best_results
+
+count_true, count_all, score, best_letter_number, best_results = main()
+print("\nFinal accuracy:", count_true, "/", count_all, '->', score, '%', 'for', best_letter_number, 'letters')
+print(best_results.head(15))
